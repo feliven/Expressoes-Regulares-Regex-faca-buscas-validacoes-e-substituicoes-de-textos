668d0259fcc90c56aa60aefcecf4237524f61225
Felipe Ventura dos Santos <felipe.ventura@bb.com.br>
2026-02-24 12:23:01 -0300

    refina busca de telefone

--- busca_telefone.js
+++ busca_telefone.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const bancoCsv = "database.csv";
 const banco = fs.readFileSync(bancoCsv, "UTF-8");
 
-const regexTelefone = /\d+/g;
+const regexTelefone = /\(\d+\)\s\d+-\d+/g;
 
 const matchTelefone = banco.match(regexTelefone);
 console.log(matchTelefone);

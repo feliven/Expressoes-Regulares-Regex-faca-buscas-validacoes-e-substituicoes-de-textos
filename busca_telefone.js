60187ca9ff659c5ce699e84441a9ee9b3547fef9
Felipe Ventura dos Santos <felipe.ventura@bb.com.br>
2026-02-24 12:39:49 -0300

    refina busca de telefones com d{n} e d{n,m}

--- busca_telefone.js
+++ busca_telefone.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const bancoCsv = "database.csv";
 const banco = fs.readFileSync(bancoCsv, "UTF-8");
 
-const regexTelefone = /\(\d+\)\s\d+-\d+/g;
+const regexTelefone = /\(\d{2}\)\s\d{4,5}-\d{4}/g;
 
 const matchTelefone = banco.match(regexTelefone);
-// console.log(matchTelefone);
+console.log(matchTelefone);
 
 const patternCel = /\(\d{2}\)\s\d{5}-\d{4}/g;
 
 const matchCelular = banco.match(patternCel);
 
-console.log(matchCelular);
+// console.log(matchCelular);

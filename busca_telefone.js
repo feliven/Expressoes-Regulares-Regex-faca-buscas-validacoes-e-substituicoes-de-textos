98b50c4d9d09c5a0acf03740183ca7c14630bc1c
Felipe Ventura dos Santos <felipe.ventura@bb.com.br>
2026-02-24 12:17:34 -0300

    first commit

---
+++ busca_telefone.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const bancoCsv = "database.csv";
+const banco = fs.readFileSync(bancoCsv, "UTF-8");
+
+const regexTelefone = /\d+/g;
+
+const matchTelefone = banco.match(regexTelefone);
+console.log(matchTelefone);

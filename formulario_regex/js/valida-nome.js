0d7e4cd0ec8a14b2c076ae9a0e1bba4288592e73
FVS <feliven@gmail.com>
2026-02-25 16:43:37 -0300

    cria validação de nome

--- formulario_regex/js/valida-nome.js
+++ formulario_regex/js/valida-nome.js
@@ -1,9 +1,16 @@
 export default function ehUmNome(campo) {
   const nome = campo.value;
-  const patternNome = /^(?!(.)\1\1)[A-Za-zÀ-ÿ -]{3,30}$/i.test(nome);
-  console.log(patternNome);
 
-  if (!patternNome) {
+  const regex1 = /^[A-Za-zÀ-ÿ -]{3,30}$/;
+  const patternNome = regex1.test(nome);
+
+  const regex2 = /^(?!(.)\1\1)[A-Za-zÀ-ÿ -]{3,30}$/i;
+  const patternNome2 = regex2.test(nome);
+
+  // console.log("patternNome:",patternNome);
+  console.log("patternNome2:", patternNome2);
+
+  if (!patternNome2) {
     campo.setCustomValidity("Não é um nome válido");
     console.log(`"${nome}" não é um nome válido`);
   }
